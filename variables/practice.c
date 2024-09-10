9a9a6387a01e61409cf3db92c5b8008050ff5726
urmma <emma.rasmussen@ucas-edu.net>
2024-09-10 00:46:53 +0000

    added all my variables

--- variables/practice.c
+++ variables/practice.c
@@ -9,5 +9,17 @@ int main(void){
     printf("%s\n", bigNumber);
     char breakfast[]= "coffee protien shake";
     printf ("%s\n", breakfast);
+    char favoriteColor []= "pink";
+    printf ("%s\n", favoriteColor);
+    char school[]= "ucas"; 
+    printf("%s\n", school);
+    char year[]= "2024";
+    printf ("%s\n",year);
+    char eyeColor[]="hazel";
+    printf("%s\n", eyeColor);
+    char age[]= "14";
+    printf("%s\n",age);
+    char favoriteSubject[]="english";
+    printf("%s\n",favoriteSubject);
 
 }
