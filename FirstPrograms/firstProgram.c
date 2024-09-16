2f1fb97066e547ef52cc125f55ede19e42990ee9
urmma <emma.rasmussen@ucas-edu.net>
2024-09-16 19:20:43 +0000

    Added the variable

--- FirstPrograms/firstProgram.c
+++ FirstPrograms/firstProgram.c
@@ -4,7 +4,7 @@ int main(void){
 char favoriteColor [30];
 printf ("what is you favorite color?: \n");
 fgets(favoriteColor, sizeof (favoriteColor), stdin);
-printf("Cute!");
+printf("Cute! %s\n" ,favoriteColor);
 return 0;
 
 }
