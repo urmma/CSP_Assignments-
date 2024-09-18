5cbb122242cf36c7ef84b1e175feb165d6cf6fad
urmma <emma.rasmussen@ucas-edu.net>
2024-09-18 00:08:03 +0000

    fixed inccorect code and made the exponants

--- expressions/exprestionsPractice.c
+++ expressions/exprestionsPractice.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <math.h>
 
 int main(void){
-    int numberOne = 7-24/8*4+6; 
+    int numbrOne = 7-24/8*4+6;
     int numbrTwo = 18/+3-7+2*5; 
     int numbrThree = 6*4+12+72/8-9; 
     int numbrFour = (17-6/2)+4*3; 
     int numbrFive = -2 *(1*4-2/2)+(6+2-3);
     int numbrSix = -1*((3-4*7)/5)-2*24/6; 
-    int numbrSeven = (3*pow (5,2)/15)+(5-pow (2,2) ); 
-    int numbrEight = (( pow  (1,4) * pow (2,2)  pow (3,3) pow (-2,5) /4);
-return 0;
+    int numbrSeven = (3* pow (5,2)/15)+(5-pow (2,2) ); 
+    int numbrEight = ( pow  (1,4) *pow (2,2)  *pow (3,3) *pow (-2,5) /4);
+    return 0;}
