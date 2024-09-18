235fbb5062d22a2feda710ff8bfc7b5813f4d525
urmma <emma.rasmussen@ucas-edu.net>
2024-09-18 19:11:06 +0000

    Added the print statments

--- expressions/exprestionsPractice.c
+++ expressions/exprestionsPractice.c
@@ -2,11 +2,22 @@
 
 int main(void){
     int numbrOne = 7-24/8*4+6;
+    printf("%d\n", numbrOne);
     int numbrTwo = 18/+3-7+2*5; 
+    printf("%d\n", numbrTwo);
     int numbrThree = 6*4+12+72/8-9; 
+    printf("%d\n", numbrThree);
     int numbrFour = (17-6/2)+4*3; 
+    printf("%d\n", numbrFour);
     int numbrFive = -2 *(1*4-2/2)+(6+2-3);
+    printf("%d\n", numbrFive);
     int numbrSix = -1*((3-4*7)/5)-2*24/6; 
+    printf("%d\n", numbrSix);
     int numbrSeven = (3* pow (5,2)/15)+(5-pow (2,2) ); 
-    int numbrEight = ( pow  (1,4) *pow (2,2)  *pow (3,3) *pow (-2,5) /4);
-    return 0;}
+    printf("%d\n", numbrSeven);
+    int numbrEight = ( pow  (1,4) *pow (2,2)  +pow (3,3)) - (pow (-2,5) /4);
+    printf("%d\n", numbrEight);
+    int numbrNine = (pow(22/2-2*5,2)+pow (4-6/6,2)); 
+    printf("%d\n", numbrNine);
+    return 0;
+    }
