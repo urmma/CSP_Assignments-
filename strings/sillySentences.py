617987a745088cff2063306912bcb289551283c1
urmma <emma.rasmussen@ucas-edu.net>
2024-09-20 19:22:11 +0000

    Made the silly sentance in python

---
+++ strings/sillySentences.py
@@ -0,0 +1,8 @@
+animal= input ("Name an animal")
+place = input ("Name a place ")
+verb = input ("Name a verb")
+
+print("The", animal)
+print("went to the " ,place)
+print("and it",verb)
+print("Whith thier friend the silly turtle")
