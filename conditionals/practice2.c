7c7804e02db350915a0eee03dacdf60ec138105d
urmma <emma.rasmussen@ucas-edu.net>
2024-10-02 19:26:07 +0000

    stuff

--- conditionals/practice2.c
+++ conditionals/practice2.c
@@ -5,9 +5,9 @@ int main(){
     if (num < 10){
     printf ("this is a sigle digit number\n"); 
     }else if (num < 100){
-        printf("this is a double diget number"); 
+        printf("this is a double diget number\n"); 
     }else{
-        printf("this is at least triple diget number"); 
+        printf("this is at least triple diget number\n"); 
     }
     return 0;
 }
