d87dc6f7018e3b77c05208fb722e2ad63a695d96
urmma <emma.rasmussen@ucas-edu.net>
2024-10-02 19:36:41 +0000

    time of day

--- conditionals/timeOfDay.c
+++ conditionals/timeOfDay.c
@@ -1,5 +1,6 @@
 #include <stdio.h> 
 #include <time.h> 
+int hour;
 
 int main ()
 {
@@ -9,6 +10,15 @@ int main ()
     time(&rawtime); 
     timeinfo = localtime (&rawtime); 
     printf("Current local time and date: %s ", asctime(timeinfo));
+    printf ("tell me the hour in military time:\n"); 
+    scanf("%d", &hour); 
+    if (hour <  12){
+    printf("bad morning"); 
+    }else if (hour<18){
+        printf("good afternoon"); 
+    }else{
+        printf ("good evening"); 
+    }
     
     return 0; 
 }
