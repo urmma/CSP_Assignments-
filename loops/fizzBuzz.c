b1a1791a03930536e7d08453ac3108acc76db00a
urmma <emma.rasmussen@ucas-edu.net>
2024-10-10 19:42:25 +0000

    made the thing

---
+++ loops/fizzBuzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+int i;
+int usr;
+char one[50]; 
+char two[50];
+char three[50]; 
+int main(){
+    printf("Give me a number");
+    scanf("%d", &usr);
+    printf("give me a short word"); 
+    scanf("%s", &one);
+    printf("give me a another short word"); 
+    scanf("%s", &two);
+    strcat(three, one); 
+    strcat(three, two);
+    //loop counts to 50
+    for(i=0;i<=usr;i++){
+     //replace numbers divisable by 3 and 5 with "fizzbuzz"
+        if(i%3==0 && i%5==0 ){
+            strcat(one,two);
+            printf("%s\n", one);
+        }else if(i%3==0){
+        //replace numbers divisable by 3 with "fizz"
+        printf("%s\n", one);
+        }else if (i%5==0){
+        // replace numbers divisable by 5 with "buzz"
+        printf("%s\n",two);
+        }else {
+        printf("%d\n", i); //print the number
+        }
+    }
+    return 0;
+}
